c4572fa0a8a460696a04943acfeadfb6e5a66d10
Shyam Mantri <iamshyam1911@gmail.com>
2025-12-10 18:45:07 +0530

    Create script.js

---
+++ JavaScript/Tabs/script.js
@@ -0,0 +1,39 @@
+let aboutButtonElement = document.getElementById("aboutButton");
+let timeToVisitButtonElement = document.getElementById("timeToVisitButton");
+let attractionsButtonElement = document.getElementById("attractionsButton");
+
+let aboutTabElement = document.getElementById("aboutTab");
+let timeToVisitTabElement = document.getElementById("timeToVisitTab");
+let attractionsTabElement = document.getElementById("attractionsTab");
+
+timeToVisitTabElement.classList.add("d-none");
+attractionsTabElement.classList.add("d-none");
+aboutButtonElement.onclick = function () {
+  aboutTabElement.classList.remove("d-none");
+  timeToVisitTabElement.classList.add("d-none");
+  attractionsTabElement.classList.add("d-none");
+
+  aboutButtonElement.classList.add("selected-button");
+  timeToVisitButtonElement.classList.remove("selected-button");
+  attractionsButtonElement.classList.remove("selected-button");
+};
+
+timeToVisitButtonElement.onclick = function () {
+  aboutTabElement.classList.add("d-none");
+  timeToVisitTabElement.classList.remove("d-none");
+  attractionsTabElement.classList.add("d-none");
+
+  aboutButtonElement.classList.remove("selected-button");
+  timeToVisitButtonElement.classList.add("selected-button");
+  attractionsButtonElement.classList.remove("selected-button");
+};
+
+attractionsButtonElement.onclick = function () {
+  aboutTabElement.classList.add("d-none");
+  timeToVisitTabElement.classList.add("d-none");
+  attractionsTabElement.classList.remove("d-none");
+
+  aboutButtonElement.classList.remove("selected-button");
+  timeToVisitButtonElement.classList.remove("selected-button");
+  attractionsButtonElement.classList.add("selected-button");
+};
